4cf4e6ffdd17aaada8e9d8cb04e2282c7ceddaab
justinw21 <justinw21@DESKTOP-1K8PKNG.localdomain>
2022-09-24 10:00:13 -0400

    Flask template

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def main():
+    search = request.form.get("search")
+    options = request.form.get("options")
+    return options
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=81, debug=True)
