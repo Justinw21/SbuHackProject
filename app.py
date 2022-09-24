514f1ad07906895caa366e7b96f44b1ae94e52cd
Compotation <46853874+Compotation@users.noreply.github.com>
2022-09-24 10:34:36 -0400

    support heroku port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,4 @@ def main():
     return options
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=81, debug=True)
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 81) || , debug=True)
