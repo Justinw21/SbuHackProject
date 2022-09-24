5e1a17fd66c70e4ae61e8c99dd471a7619230734
justinw21 <justinw21@DESKTOP-1K8PKNG.localdomain>
2022-09-24 18:14:44 -0400

    commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from twilio.rest import Client
 import os
 
 app = Flask(__name__)
@@ -12,7 +13,22 @@ def main():
     search = request.form.get("search")
     options = request.form.get("options")
     number = request.form.get("number")
+
+    
+
+    account_sid = "AC24785ce0decacf8e0d2fca5362a64c09"
+    auth_token  = "a1430f49bfcb2ace6f183c9a1fbdea18"
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        to="+1" + number, 
+        from_="++15618162572",
+        body="Hello from Python!")
+
+    print(message.sid)
     return search + " " + options + " " + number
+
 
 if __name__ == "__main__":
     is_prod = os.environ.get('IS_HEROKU', None)
